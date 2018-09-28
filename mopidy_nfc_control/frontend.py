2bab151022af0f2dd572d401c0fa46dade73c546
Willi Meierhof <meierhofwilli@gmail.com>
2018-09-28 20:45:46 +0200

    receive the ids and print them out

--- mopidy_nfc_control/frontend.py
+++ mopidy_nfc_control/frontend.py
@@ -1,11 +1,15 @@
 import logging
 import traceback
 
+import time
+
 from mopidy import core
+import nxppy
 
 import pykka
 
 logger = logging.getLogger(__name__)
+mifare = nxppy.Mifare()
 
 
 class NfcControl(pykka.ThreadingActor, core.CoreListener):
@@ -13,3 +17,19 @@ class NfcControl(pykka.ThreadingActor, core.CoreListener):
     def __init__(self, config, core):
         super(NfcControl, self).__init__()
         self.core = core
+        self.lastId = None
+
+    def _getIds():
+        while True:
+            # reading the card id
+            try:
+                uid = mifare.select()
+                logging.info("Received the following data: {}".format(uid))
+                if uid != self.lastId:
+                    self.lastId = uid
+
+            except nxppy.SelectError as se:
+                # SelectError is raised if no card is in the field.
+                logging.debug("Had an issue selecting the id: {}".format(se))
+
+            time.sleep(0.5)
