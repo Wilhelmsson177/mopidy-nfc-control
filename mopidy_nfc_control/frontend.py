564ff03e265928fa14baee18a0b9ab0e8eb4104c
Willi Meierhof <meierhofwilli@gmail.com>
2018-09-28 20:28:53 +0200

    updated frontend and init for nfcControl

--- mopidy_nfc_control/frontend.py
+++ mopidy_nfc_control/frontend.py
@@ -9,4 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class NfcControl(pykka.ThreadingActor, core.CoreListener):
-    pass
+
+    def __init__(self, config, core):
+        super(NfcControl, self).__init__()
+        self.core = core
