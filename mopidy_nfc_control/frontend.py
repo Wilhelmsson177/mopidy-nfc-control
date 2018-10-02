4ab65774527a2cbf8d1316309d0ba6305a54137b
Wilhelmsson177 <meierhofwilli@gmail.com>
2018-10-02 13:50:13 +0200

    Added new callback function

--- mopidy_nfc_control/frontend.py
+++ mopidy_nfc_control/frontend.py
@@ -23,8 +23,12 @@ class NfcControl(pykka.ThreadingActor, core.CoreListener):
         self.nfcTagMonitor = NfcTagMonitor()
         self.nfcTagMonitor.RegisterControlTagCallback(self.Control)
         self.nfcTagMonitor.RegisterUriTagCallback(self.PlaybackonUri)
+        self.nfcTagMonitor.RegisterNfcTagRemovedCallback(self.TagRemoved)
         start_new_thread(self.nfcTagMonitor.Run,())
 
+    def TagRemoved(self):
+        logger.debug('Tag has been removed')
+    
     def Control(self, input=None):
         logger.info('Received {} control.'.format(input))
 
