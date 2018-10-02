963ab48004a853f5e700439a28e2ba06f0f92680
Wilhelmsson177 <meierhofwilli@gmail.com>
2018-10-02 13:27:15 +0200

    Added callback for NFC tag removed.
    If card is removed, the callback will be send. In case of NfcTag holder and no swiping.

--- mopidy_nfc_control/NfcTagMonitor.py
+++ mopidy_nfc_control/NfcTagMonitor.py
@@ -16,13 +16,16 @@ class NfcTagMonitor(object):
     '''The tag monitor class definition.
     '''
     
-    def __init__(self):
+    def __init__(self, nfc_tag_hold=True):
         """
         Constructor.
         """
         self.lastTag = None
         self.uriTagCallback = None
         self.controlTagCallback = None
+        self.nfcTagRemovedCallback = None
+        self.nfcTagHold = nfc_tag_hold
+        self.tagRemoved = True
         self.mifare = nxppy.Mifare()
 
     def Run(self):
@@ -47,17 +50,24 @@ class NfcTagMonitor(object):
                             logger.info("detected unknown type")
                             logger.info('TYPE: {}'.format(record.type))
                             self.controlTagCallback()
-
+                else:
+                    if self.nfcTagHold and self.tagRemoved:
+                       self.controlTagCallback('RESUME')
+                self.tagRemoved = False
             except nxppy.SelectError as se:
                 # SelectError is raised if no card is in the field.
-                logger.debug("Had an issue selecting the id: {}".format(se))
+                logger.debug("Had an issue selecting the id. Probably the NFC Tag has been removed: {}".format(se))
+                if self.nfcTagHold:
+                    self.nfcTagRemovedCallback()
+                    self.tagRemoved = True
 
-            time.sleep(0.5)
+            time.sleep(0.1)
 
     def RegisterUriTagCallback(self, callback):
         self.uriTagCallback = callback
     
     def RegisterControlTagCallback(self, callback):
         self.controlTagCallback = callback
-
-
+        
+    def RegisterNfcTagRemovedCallBack(self, callback):
+        self.nfcTagRemovedCallback = callback
