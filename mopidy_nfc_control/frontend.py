a8e654e3936608486301ca73b71a307ec7a27b57
Willi Meierhof <meierhofwilli@gmail.com>
2018-10-20 20:45:38 +0200

    indexsafe

--- mopidy_nfc_control/frontend.py
+++ mopidy_nfc_control/frontend.py
@@ -64,7 +64,5 @@ class NfcControl(pykka.ThreadingActor, core.CoreListener):
             self.core.tracklist.clear()
             tracks = self.core.tracklist.add(uris=[uri])
             logger.debug("{}Received tracks: {}".format(__logprefix__, tracks.get()))
-            tl_track = self.core.tracklist.get_tl_tracks().get()[0]
-            logger.debug("{}Newest track: {}".format(__logprefix__, tl_track))
             self.core.playback.play()
             logger.debug("{}State: {}".format(__logprefix__, self.core.playback.get_state().get()))
